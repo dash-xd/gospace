ea399d73e55b8d27926dfc6a707d697b8c84acfa
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-08-30 21:07:44 -0700

    Serve stable modular worker handler

--- gospace/cmd/api/main.go
+++ gospace/cmd/api/main.go
@@ -10,31 +10,30 @@ import (
 )
 
 func main() {
-	port, key := parseArgs()
+	port, initial := parseArgs()
+	if initial != "" {
+		if err := gospace.Activate(initial); err != nil {
+			fmt.Fprintf(os.Stderr, "activate router %q: %v\n", initial, err)
+			os.Exit(1)
+		}
+	}
+
 	addr := fmt.Sprintf(":%d", port)
-
-	fmt.Printf("Go Server is listening on http://localhost%s\n", addr)
-
-	err := http.ListenAndServe(addr, http.HandlerFunc(gospace.GetRouter(key)))
-	if err != nil {
+	fmt.Printf("gospace worker is listening on http://localhost%s\n", addr)
+	if err := http.ListenAndServe(addr, http.HandlerFunc(gospace.Main)); err != nil {
 		fmt.Fprintf(os.Stderr, "server error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
 func parseArgs() (int, string) {
-	defaultPort := 6060
-	defaultKey := "serve"
-
-	portPtr := flag.Int("port", defaultPort, "Port for the server to listen on")
-	keyPtr := flag.String("key", defaultKey, "Key that determines which router to run from gospace")
-
+	portPtr := flag.Int("port", 6060, "Port for the server to listen on")
+	initialPtr := flag.String("router", "", "Optional pre-registered router to activate at startup")
 	flag.Parse()
 
 	if *portPtr <= 0 || *portPtr > 65535 {
-		fmt.Fprintf(os.Stderr, "Invalid port: %d. Using default port: %d\n", *portPtr, defaultPort)
-		*portPtr = defaultPort
+		fmt.Fprintln(os.Stderr, "port must be between 1 and 65535")
+		os.Exit(2)
 	}
-
-	return *portPtr, *keyPtr
+	return *portPtr, *initialPtr
 }
